1e991b07cf3cbbc20e78528074589848149b2db5
Christopher Czach <ChrisCzach@Christophers-MacBook-Pro.socal.rr.com>
2018-10-24 17:14:53 -0700

    Updates

---
+++ precache-manifest.1403ac6e44ce44d797bca94518fa2305.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3df533f9745479d3e7e41ea13f2fe97e",
+    "url": "/winnie-hunt/static/media/grass2.3df533f9.jpg"
+  },
+  {
+    "revision": "278d163cbd2a261007fd",
+    "url": "/winnie-hunt/static/css/main.47588bc6.chunk.css"
+  },
+  {
+    "revision": "3686772e405f15574832",
+    "url": "/winnie-hunt/static/js/1.3686772e.chunk.js"
+  },
+  {
+    "revision": "66365268520036f24669",
+    "url": "/winnie-hunt/static/js/runtime~main.66365268.js"
+  },
+  {
+    "revision": "5fc7664deeab99c8ff24bb15ae63abfb",
+    "url": "/winnie-hunt/static/media/Flaticon.5fc7664d.eot"
+  },
+  {
+    "revision": "278d163cbd2a261007fd",
+    "url": "/winnie-hunt/static/js/main.278d163c.chunk.js"
+  },
+  {
+    "revision": "72bd4c6e810be6bb90a9f8db2f63342e",
+    "url": "/winnie-hunt/static/media/Flaticon.72bd4c6e.woff"
+  },
+  {
+    "revision": "3bac97920f8a53c97033219cc61a7dd0",
+    "url": "/winnie-hunt/static/media/Flaticon.3bac9792.ttf"
+  },
+  {
+    "revision": "90069d82911bbec35f1c1314956ea6f6",
+    "url": "/winnie-hunt/static/media/Flaticon.90069d82.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/winnie-hunt/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "81df95a96540969e6ed9299076ddedc7",
+    "url": "/winnie-hunt/index.html"
+  }
+];
